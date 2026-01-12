5eeaab384dbb122748cb237f08da19007452f7f7
Zaur_Magomedov <zaurmag411@gmail.com>
2026-01-12 23:50:07 +0300

    Refactor: decomposition of logic and components

---
+++ src/components/ThingGrid.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="thing-grid">
+    <slot />
+
+    <p v-if="info" class="thing-grid__info">
+      {{ info }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  info?: string
+}>()
+</script>
+
+<style scoped lang="scss">
+.thing-grid {
+  border: 4px solid $color-dark;
+  padding: 10px;
+  text-align: center;
+  font-size: 14px;
+  display: grid;
+  grid-gap: 15px;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  align-content: start;
+
+  &__info {
+    font-size: 16px;
+    grid-column: 1 / -1;
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use '/src/assets/variables' as *;
+        `,
+      },
+    },
+  },
 })
